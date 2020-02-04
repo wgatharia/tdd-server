215bff76a086de6ac7f50183feafce21cecf03a3
William Gatharia <wgatharia@yahoo.com>
2020-02-04 08:27:33 -0800

    moved reset to afterEach hook.

--- src/db.test.js
+++ src/db.test.js
@@ -3,6 +3,10 @@ import { getUserByUsername } from './db';
 import { getDatabaseData, setDatabaseData, resetDatabase } from './test-helpers';
 
 describe('getUserByUsername', () => {
+    afterEach('reset the database', async ()=> {
+        await resetDatabase();
+    })
+
     it('get the correct user from the database given a username', async () => {
         const fakeData = [{
             id: '123',
@@ -18,7 +22,6 @@ describe('getUserByUsername', () => {
 
         const actual = await getUserByUsername('abc');
         const finalDBState = await getDatabaseData('users');
-        await resetDatabase();
 
         const expected = {
             id: '123',
